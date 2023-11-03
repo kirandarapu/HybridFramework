0403312cc5fd9b7c00039696de4fa30cb0e17f2e
kirandarapu <naveendarapu108@gmail.com>
2023-11-03 15:59:41 +0530

    this is my modification as per your suggestion

--- src/test/java/Testcases/Examples001.java
+++ src/test/java/Testcases/Examples001.java
@@ -11,11 +11,21 @@ public class Examples001 {
 		System.out.println(b-a);
 	}
 	
+	public void mult() {
+		System.out.println(a*b);
+	}
+	
+	public void Div() {
+		System.out.println(b/a);
+	}
+	
 	public static void main(String[] args) {
 		
 		Examples001  e=new Examples001 ();
 		e.sum();
 		e.Sub();
+		e.mult();
+		e.Div();
 	}
 
 }
